73fd9e8fcf4817a32f6f0055a0d8ddfc9f4a51d2
acarrillo2019 <alric21@aol.com>
2020-02-27 16:38:20 -0700

    Added table

--- bamazon.sql
+++ bamazon.sql
@@ -12,3 +12,19 @@ CREATE TABLE products(
   product_sales DECIMAL(10,2) DEFAULT 0.00,
   PRIMARY KEY (id)
 );
+
+CREATE TABLE departments(
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) ,
+  over_head_costs DECIMAL(10,2) DEFAULT 0.00,
+  PRIMARY KEY (department_id)
+);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Equipment", 500.00);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Apparel", 200.00);
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Shoes", 250.00);
