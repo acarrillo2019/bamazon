c02ef352637782e94dbb6bde598d438a4930929b
acarrillo2019 <alric21@aol.com>
2020-02-28 22:18:05 -0700

    Added to JS

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,3 +14,70 @@ var sqlConfig = {
     password: "root",
     database: "bamazon_DB"
 };
+
+// create the connection information for the sql database
+var connection = mysql.createConnection(sqlConfig);
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+
+    common.printHeader("Welcome to Bamazon Tennis Super Store!","blue");
+    purchaseItem();
+});
+
+function purchaseItem() {
+    // Query the DB for all items in store inventory
+    connection.query("SELECT * FROM products", function(err, results) {
+        if (err) throw err;
+
+        common.displayItems(results, "blue","customer");
+
+        var item = 0;
+
+        // Prompt customer to make purchase selection
+        inquirer.prompt([
+            {
+                name: "itemNumber",
+                type: "input",
+                message: chalk.yellow("Which item would you like to purchase?"),
+                validate: function(num){
+                    // Check if item number is valid, returns object if found, else returns -1 if not found
+                    // Note: 'item' is an object with the specific record if found in the db. Could use the variable 'item'
+                    // to access the values instead of querying the db later in updateInventory. Didn't know if it was best practice
+                    // to pass this variable into the next function, or perform a new query to get the requested record.
+                    // Probably the later, could modify updateInventory to pass the table name into the function, then the function
+                    // would be generic and could be used to udpate inventories in various tables.
+                    item = results.find(x => x.id === parseInt(num));
+                    if (item) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ])
+        .then(function(ans1) {
+            // Split into two prompt statements. Use the item number from the first response to check if there is sufficient inventory, 
+            // then proceed with purchase. Otherwise, ask customer to enter another amount.
+
+            // Prompt user to select quantity
+            inquirer.prompt([
+                {
+                    name: "qty",
+                    type: "input",
+                    message: chalk.yellow("How many would you like to purchase?"),
+                    validate: function(num){
+                        if (parseInt(num) < item.stock_quantity) {
+                            return true;
+                        }
+                        console.log(chalk.red("\rInsufficient quantity in stock, please select another amount."));
+                        return false;
+                    }
+                }
+            ])
+            .then(function(ans2) {
+                updateInventory(ans1.itemNumber, parseInt(ans2.qty));
+            })
+        });
+    });
+}
