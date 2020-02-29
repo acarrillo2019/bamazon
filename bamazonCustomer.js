b9f508bc9267af70a97f83950482e56081d1db01
acarrillo2019 <alric21@aol.com>
2020-02-28 22:16:56 -0700

    Created customer js

---
+++ bamazonCustomer.js
@@ -0,0 +1,16 @@
+
+// NPM modules
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var chalk = require("chalk");
+
+// Common functions used in bamazonCustomer/Manager/Supervisor
+var common = require("./common.js");
+
+var sqlConfig = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon_DB"
+};
